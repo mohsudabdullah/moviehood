659e535c64ef7d049ee7092ca18b310b56a66dc6
Mohsud Abdullah <mohsudabdullah@gmail.com>
2019-05-26 22:52:39 +0300

    innit

---
+++ js/body.js
@@ -0,0 +1,13 @@
+class Hello extends React.Component {
+    
+    render() {
+      return <div>
+          Hello {this.props.toWhat}
+          </div>;
+    }
+  }
+  
+  ReactDOM.render(
+    <Hello toWhat="World" />,
+    document.getElementById('app')
+  );
